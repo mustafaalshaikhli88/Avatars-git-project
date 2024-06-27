1510b25f287f2252c0a20cda32e28dd6f232d6c9
mustafaalshaikhli88 <mustafaalshaikhli88@gmail.com>
2024-06-26 21:27:30 -0400

    My third time pusing code

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -3,6 +3,8 @@ package org.example;
 public class Testing {
 
     public static void main(String[] args) {
-        System.out.println("My third git class");
+        System.out.println("Hello world");
+        System.out.println("I AM HAVING THE TIME OF MY LIFE ");
+        System.out.println("I CAN NOT WAIT TO GET A JOB");
     }
 }
